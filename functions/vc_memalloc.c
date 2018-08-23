0030bbb4dfabbb9b28cc22e6858181ad85e278b6
Bianca Campos <biancancampos@gmail.com>
2018-08-22 19:28:45 -0700

    Add vc_memalloc.c

---
+++ functions/vc_memalloc.c
@@ -0,0 +1,30 @@
+/* ************************************ */
+/*                                      */
+/* vc_memalloc.c                        */
+/*                                      */
+/* By: Bianca                           */
+/*                                      */
+/* ************************************ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void *vc_memalloc(size_t size)
+{
+    int i;
+    int *ptr_block;
+
+    i = 0;
+    while (i < size)
+    {
+        ptr_block = malloc(sizeof(int));
+        if (ptr_block == NULL)
+        {
+            return NULL;
+        }
+        else
+        {
+            *ptr_block = 0;
+            i++;
+        }
+    }
+}
