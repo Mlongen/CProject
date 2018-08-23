aba3c96b43dd156ca593d25fd110ea0760577e52
Marcelo Longen <marcelolongen@gmail.com>
2018-08-23 11:01:16 -0700

    add makefile

--- functions/vc_itoa.c
+++ functions/vc_itoa.c
@@ -5,58 +5,49 @@
 /* By:Javier                            */
 /*                                      */
 /* ************************************ */
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int len (long nb)
+int len(long nb)
 {
     int len;
     len = 0;
-    if(nb < 0)
+    if (nb < 0)
     {
         nb = nb * -1;
         len++;
     }
-    while(nb > 0)
+    while (nb > 0)
     {
-        nb =nb/10;
-
+        nb = nb / 10;
         len++;
     }
-    return(len);
+    return (len);
 }
-    char *vc_itoa(int nb)
-    {
-        char *str;
-        long n;
-        int i;
-        n = nb;
-        i=len(n);
-        if(!(str =(char*)malloc(sizeof(char)* (i+1))))
+char *vc_itoa(int n)
+{
+    char *str;
+    long nb;
+    int i;
+    nb = n;
+    i = len(nb);
+    if (!(str = (char *)malloc(sizeof(char) * (i + 1))))
         return NULL;
-        str[i--] = '\0';
-        if(n == 0)
-        {
-            str[0] = '-';
-            return (str);
-        }
-        if(n < 0)
-        {
-            str[0] = '-';
-            n = n * -1;
-        }
-        while (n > 0)
-        {
-            str[i] = 48 + (n % 10 );
-            n= n / 10;
-            i--;
-        }
-        return(str);
+    str[i--] = '\0';
+    if (nb == 0)
+    {
+        str[0] = '-';
+        return (str);
     }
-    
-
-
-
-
-
-
+    if (nb < 0)
+    {
+        str[0] = '-';
+        nb = nb * -1;
+    }
+    while (nb > 0)
+    {
+        str[i] = 48 + (nb % 10);
+        nb = nb / 10;
+        i--;
+    }
+    return (str);
+}
